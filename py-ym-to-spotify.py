2071da2e5fe11930b14f412c95addfce77d20274
Ilnur Saifutdinov <ilnursoft@gmail.com>
2020-07-26 22:08:01 +0300

    небольшой рефакторинг

--- py-ym-to-spotify.py
+++ py-ym-to-spotify.py
@@ -1,61 +1,28 @@
-import os
 import time
-import json
-import requests
 from bs4 import BeautifulSoup
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 
-# константы
-ym_link = 'https://music.yandex.ru'
-# END константы
-
 # трек 
-class Track:
-    def __init__(self):
-        self.song = {}
-        self.author = {}
+# class Track:
+#     def __init__(self):
+#         self.song = {}
+#         self.author = {}
 
 
-def save_to_html(filename, htmlpage):
-    with open(filename, 'w') as f:
-        f.write(htmlpage)
-
-
-def open_html(filename):
-    result = None
-    with open(filename, 'r') as f:
-        result = f.read()
-    return result
-
-
-def open_playlist_ym(urls, outfilename="my_playlist.html"):
-    session = requests.Session()
-    r = session.get(urls)
-    print(r.status_code)
-
-    # TODO: нужно сделать пролистывание страницы до конца чтобы все песни загрузились, а так загружается 150 шт
-
-    if (r.status_code == 200):
-        # print(r.headers)
-        # print(r.content)
-        # print(r.text)
-        soup = BeautifulSoup(r.text, 'html.parser')
-        save_to_html(outfilename, soup.prettify())
-
-
-def open_playlist_ym_selenuim(urls, outfilename="my_playlist.html", hidden=True):
+# сохранение всех треков из плейлиста Yandex Music
+def open_playlist_ym_selenuim(urls, outfilename="my_playlist.html", hidden=True, debug=False):
     # настройки скрытого режима хрома
     WINDOW_SIZE = "1920,1080"
     chrome_options = Options()
-    if (hidden):
+    if hidden:
         chrome_options.add_argument("--headless")
     chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
     # END настройки скрытого режима хрома
 
-    driver = webdriver.Chrome(chrome_options=chrome_options, executable_path='./drv/chromedriver')
+    driver = webdriver.Chrome(options=chrome_options, executable_path='./drv/chromedriver')
     driver.get(urls)
 
     i = 0
@@ -63,18 +30,21 @@ def open_playlist_ym_selenuim(urls, outfilename="my_playlist.html", hidden=True)
     last_song_old = ""
     my_playlist = ""
     while elem_exit:
-        print("Шаг ", i)
+        if debug:
+            print("Шаг ", i)
         elem_sidebar = driver.find_elements_by_css_selector('body')
         elem_sidebar[0].send_keys(Keys.PAGE_DOWN)
         # TODO: надо убрать и сделать чтобы selenium дожидался сам загрузки
         time.sleep(5)
 
-        print("Elem SideBar = ", elem_sidebar)
+        if debug:
+            print("Elem SideBar = ", elem_sidebar)
         i = i + 1
         elem_playlist = driver.find_elements_by_css_selector('div.d-track.typo-track.d-track_selectable')
-        print('LEN elem_playlist = ', len(elem_playlist))
+
         last_song = elem_playlist[-1].text
-        print(last_song)
+        if debug:
+            print(last_song)
         if last_song == last_song_old:
             elem_exit = False
         else:
@@ -82,30 +52,23 @@ def open_playlist_ym_selenuim(urls, outfilename="my_playlist.html", hidden=True)
             for el in elem_playlist:
                 my_playlist = my_playlist + el.get_attribute("outerHTML")
 
-    print("Достигли конца страницы")
-    save_to_html(outfilename, my_playlist)
+    if debug:
+        print("Достигли конца страницы")
+
+    with open(outfilename, 'w') as f:
+        f.write(my_playlist)
 
 
-if __name__ == "__main__":
-    urls = f"https://music.yandex.ru/users/IlnurSoft/playlists/3"
-    filename = "playlist.html"
-
-    open_playlist_ym_selenuim(urls, filename, True)
-
-    playlist_ym = open_html(filename)
-
-    # print(playlist_ym)
-
+# выделение информации о треках
+def get_tracks_from_htmlpage_yandexmusic(playlist_ym, debug=False):
+    ym_link = 'https://music.yandex.ru'
     soup = BeautifulSoup(playlist_ym, 'html.parser')
     music = soup.select('div.d-track.typo-track.d-track_selectable')
 
-    print(len(music))
-    # music_html = music.prettify()
-    # print(music_html)
-    save_to_html('music.html', music[0].prettify())
+    if debug:
+        with open('music.html', 'w') as f:
+            f.write(music[0].prettify())
 
-    # item_music = music[0]   
-    tracks = []
     tracks_dict = {}  # ключ - это исполнитель, значение - массив с названиями треков-песен
     for item_music in music:
         author = {}
@@ -115,36 +78,38 @@ if __name__ == "__main__":
         song_d = item_music.select_one('div.d-track__name')
         song['name'] = song_d['title']
         song['link'] = ym_link + song_d.a['href']
-        # print(song)
 
         # информация об артисте
         author_d = item_music.select_one('span.d-track__artists')
-        # print(author_d)
         # TODO: сделать чтобы выделялись несколько исполнителей, если они есть
         author['name'] = author_d.a['title']
         author['link'] = ym_link + author_d.a['href']
-        # print(author)
-        # print(author['name'], song['name'])
-        # print(tracks_dict)
         if author['name'] in tracks_dict:
             ss = tracks_dict[author['name']]
-            # print("ss = ", ss)
             tracks_dict[author['name']].add(song['name'])
         else:
             tracks_dict[author['name']] = {song['name']}
 
-    # print(tracks_dict)
-    print(len(tracks_dict))
+    return tracks_dict
+
+
+if __name__ == "__main__":
+    # urls = f"https://music.yandex.ru/users/IlnurSoft/playlists/3"
+    urls= "https://music.yandex.ru/users/yamusic-daily/playlists/23758179"
+    filename = "playlist.html"
+
+    open_playlist_ym_selenuim(urls, filename, hidden=True, debug=False)
+
+    playlist_ym = None
+    with open(filename, 'r') as f:
+        playlist_ym = f.read()
+
+    tracks_dict = get_tracks_from_htmlpage_yandexmusic(playlist_ym, debug=False)
 
     #  сохранение результата в файл
     with open("my_playlist.csv", "w") as f:
         f.write('artist%track\n')
         for key, el in tracks_dict.items():
-            # songs = ";".join(el)
             for song in el:
                 row = f"{key}%{song}\n"
                 f.write(row)
-
-
-
-
